f6e9dd878aab5f1485169e3d73e98d8771cf6e26
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-08-28 23:36:30 +0200

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'qrcode';
+  imageUrl: string = '';
+
+  constructor() {}
+  download: string = '';
+  size: string = '';
+  text: string = '';
+  format: string = '';
+  bgcolor: string = '';
+  buttonGenerate() {
+    this.imageUrl =
+      'http://api.qrserver.com/v1/create-qr-code/?' +
+      this.size +
+      'x' +
+      this.size +
+      '&data=' +
+      this.text +
+      '&format=' +
+      this.format +
+      '&bgcolor=' +
+      this.bgcolor;
+    this.download = 'Click image to download';
+  }
 }
