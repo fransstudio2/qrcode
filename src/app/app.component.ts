cdb69e3513d17457b7c7025a9971ff319df00516
Dara Ganesha <daraganesha@Daras-MBP.fritz.box>
2020-08-29 16:31:17 +0200

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,25 +8,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imageUrl: string = '';
-
-  constructor() {}
-  download: string = '';
-  size: string = '';
+  message: string = '';
+  size: number;
   text: string = '';
   format: string = '';
   bgcolor: string = '';
+  color: string = '';
+  isGenerated: boolean = false;
+  imageData: string = '';
+
+  constructor(private http: HttpClient) {}
+
   buttonGenerate() {
-    this.imageUrl =
-      'http://api.qrserver.com/v1/create-qr-code/?' +
-      this.size +
-      'x' +
-      this.size +
-      '&data=' +
-      this.text +
-      '&format=' +
-      this.format +
-      '&bgcolor=' +
-      this.bgcolor;
-    this.download = 'Click image to download';
+    if (this.text === '') {
+      this.message = 'Please enter the text';
+      this.imageData = '';
+      this.isGenerated = false;
+    } else if (this.size < 50 || this.size > 1000 || isNaN(this.size)) {
+      this.message = 'Size should be number between 50 to 1000';
+      this.imageData = '';
+      this.isGenerated = false;
+    } else {
+      this.imageUrl =
+        'http://api.qrserver.com/v1/create-qr-code/?size=' +
+        this.size +
+        'x' +
+        this.size +
+        '&data=' +
+        this.text +
+        '&format=' +
+        this.format +
+        '&bgcolor=' +
+        this.bgcolor +
+        '&color=' +
+        this.color;
+      this.message = '';
+
+      this.http
+        .get(this.imageUrl, { responseType: 'blob' })
+        .subscribe((data) => {
+          const reader = new FileReader();
+          reader.addEventListener(
+            'load',
+            () => {
+              this.imageData = reader.result as string;
+            },
+            false
+          );
+          reader.readAsDataURL(data);
+        });
+      this.isGenerated = true;
+      console.log(this.imageData + '1');
+    }
   }
 }
